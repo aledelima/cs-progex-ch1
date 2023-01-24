21f56a4cd808e878c6864a2a95e52416e6806497
Alessandro Lima <alessandroslima@hotmail.com>
2023-01-23 22:40:08 -0800

    Solution for ex2

---
+++ ex2/ex2.cs
@@ -0,0 +1,28 @@
+//Microsoft (R) Visual C# Compiler version 3.4.0-beta4-19562-05 (ff930dec)
+//Copyright (C) Microsoft Corporation. All rights reserved.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            
+            Console.WriteLine("*********************************************************");
+            Console.WriteLine("*\tProgram Assignment #2\t\t\t\t*");
+            Console.WriteLine("*\tDeveloper: Alessandro de Lima\t\t\t*");
+            Console.WriteLine("*\tDate Submitted: January, 23th\t\t\t*");
+            Console.WriteLine("*\tPurpose: Code Practice\t\t\t\t*");
+            Console.WriteLine("*********************************************************");
+            Console.WriteLine();
+        }
+    }
+}
