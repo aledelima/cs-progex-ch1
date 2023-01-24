78d9a51c2a447c14a86f3aead5f78821fea39b46
Alessandro Lima <alessandroslima@hotmail.com>
2023-01-23 22:40:28 -0800

    Solution for ex3

---
+++ ex3/ex3.cs
@@ -0,0 +1,31 @@
+//Microsoft (R) Visual C# Compiler version 3.4.0-beta4-19562-05 (ff930dec)
+//Copyright (C) Microsoft Corporation. All rights reserved.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            
+            Console.WriteLine("##\t\t\t\t\t\t\t\t##");
+            Console.WriteLine("\t##\t\t\t\t\t\t##");
+            Console.WriteLine("\t\t##\t\t\t\t##");
+            Console.WriteLine("\t\t\t##\t\t##");
+            Console.WriteLine("\t\t\t\t##");
+            Console.WriteLine("\t\t\t##\t\t##");
+            Console.WriteLine("\t\t##\t\t\t\t##");
+            Console.WriteLine("\t##\t\t\t\t\t\t##");
+            Console.WriteLine("##\t\t\t\t\t\t\t\t##");
+            Console.WriteLine("");
+        }
+    }
+}
