0f7ad3e65c64b10811bb84e38b91a6266d8b3470
Alessandro Lima <alessandroslima@hotmail.com>
2023-01-23 22:41:08 -0800

    Solution for ex7

---
+++ ex7/ex2.cs
@@ -0,0 +1,29 @@
+//Microsoft (R) Visual C# Compiler version 3.4.0-beta4-19562-05 (ff930dec)
+//Copyright (C) Microsoft Corporation. All rights reserved.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("*|\tName: Alessandro de Lima\\\t\t\t *");
+            Console.WriteLine("* \tHometown: Rio de Janeiro\\\t\t\t *");
+            Console.WriteLine("* \tMajor: CS\\\t\t\t\t\t *");
+            Console.WriteLine("* \tHobby: Working Out\\\t\t\t\t *");
+            Console.WriteLine("* \tFavorite Activity: Traveling\\\t\t\t|*");
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine();
+        }
+    }
+}
